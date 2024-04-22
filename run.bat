c89a84165d43c5346d472c0f78365f52d96dcd08
kn514 <148934231+kn514@users.noreply.github.com>
2024-04-22 20:17:44 +0900

    remove debug code

--- run.bat
+++ run.bat
@@ -1,4 +1,4 @@
-rem powershell -executionpolicy remotesigned -file .\setup_pyenv_and_package.ps1
-start poetry run python -m pytest_dashboard.tolly .\test_root\progress .\test_root\entire-progress.yaml
+powershell -executionpolicy remotesigned -file .\setup_pyenv_and_package.ps1
+start poetry run python -m pytest_dashboard.tolly .\test_root\progress .\test_root\entire-progress.yaml --notification=True
 powershell -executionpolicy remotesigned -file .\run_pytest.ps1
 pause
