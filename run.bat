9bc0a0e9033e82d162a6a564fd09b0793ee65440
kn514 <148934231+kn514@users.noreply.github.com>
2024-05-30 15:45:21 +0900

    remove unnecessary codes

--- run.bat
+++
@@ -1,4 +0,0 @@
-powershell -executionpolicy remotesigned -file .\setup_pyenv_and_package.ps1
-start poetry run tally-pytest .\test_root\progress .\test_root\entire-progress.yaml --notification=True
-powershell -executionpolicy remotesigned -file .\run_pytest.ps1
-pause
